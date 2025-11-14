f84a4ed97020432d6f16a381adbce2d8d1b20bfa
Miichael Ajayi <michaelajayi1995@gmail.com>
2025-11-14 07:20:20 -0500

    Created a funtion to select fruit and also limit the selection

--- JS/index.js
+++ JS/index.js
@@ -3,4 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const smoothieOutput = document.getElementById('smoothieOutput');
     const fruitCheckboxes = document.querySelectorAll('input[name="fruits"]');
 
-    
+    // Limit fruit selection to 4
+    fruitCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function() {
+            const checkedFruits = document.querySelectorAll('input[name="fruits"]:checked');
+            
+            if (checkedFruits.length > 4) {
+                this.checked = false;
+                alert('You can only select up to 4 fruits!');
+            }
+        });
+    }); 
+
