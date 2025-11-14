99271a9a976e12cc3ac3347f2fab35f1e45178a2
Miichael Ajayi <michaelajayi1995@gmail.com>
2025-11-14 07:18:51 -0500

    created a function to fully load DOM

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('smoothieForm');
+    const smoothieOutput = document.getElementById('smoothieOutput');
+    const fruitCheckboxes = document.querySelectorAll('input[name="fruits"]');
+
+    
